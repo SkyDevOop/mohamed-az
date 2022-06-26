e24cbae9078d8a8746f0edd0cec372fb2fa484b0
issam bourouail <issambourouail9@gmail.com>
2022-06-27 00:39:20 +0200

    first commit test

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+// For social media and contact in media (max-width 992px)
+const round = document.querySelector(".round")
+const info = document.querySelector(".info-header")
+round.addEventListener("click", (eo) => {
+    info.classList.toggle("active-social")
+})
+// For Nav Bar in media Max-width 992px
+const bar = document.getElementById("bar")
+const item = document.querySelector(".item")
+console.log(item);
+bar.addEventListener("click", (eo) => {
+    item.classList.toggle("active-nav")
+})
